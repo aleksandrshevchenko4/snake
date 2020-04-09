5df6fafbfc1a51c3d09363d6ace5e1a738651afa
aleksandr.shevchenko <sanjkashevcenko@gmail.com>
2020-04-09 14:03:50 +0300

    Lesson-2

---
+++ snake/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+
+            Draw(int x1, int y1, char sym1)
+
+            int x2 = 4;
+            int y2 = 5;
+            char sym2 = '#';
+
+            Draw(int x2, int y2, char sym2)
+
+            Console.ReadLine();
+        }
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
