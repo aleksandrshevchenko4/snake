d1b20b0572a41ce3e1f4d4f7a99dbddc812a876b
aleksandr.shevchenko <sanjkashevcenko@gmail.com>
2020-04-09 15:24:43 +0300

    Lesson-6

--- snake/Point.cs
+++ snake/Point.cs
@@ -16,7 +16,12 @@ namespace snake
         {
         }
 
-        public Poit
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
 
         public void Draw()
         {
