64ee8d5b5e60263a64f1b451864d04625e0cd79d
aleksandrshevchenko4 <62281789+aleksandrshevchenko4@users.noreply.github.com>
2020-06-04 22:23:56 +0300

    Update Point.cs

--- snake/Point.cs
+++ snake/Point.cs
@@ -1,10 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace snake
+namespace Snake
 {
     class Point
     {
@@ -14,8 +12,8 @@ namespace snake
 
         public Point()
         {
-        }
 
+        }
         public Point(int _x, int _y, char _sym)
         {
             x = _x;
@@ -23,35 +21,36 @@ namespace snake
             sym = _sym;
         }
 
-        public Point(Point p)
+        public Point(Point point)
         {
-            x = p.x;
-            y = p.y;
-            sym = p.sym;
+            x = point.x;
+            y = point.y;
+            sym = point.sym;
         }
 
         public void Move(int offset, Direction direction)
         {
-            if (direction == Direction.RIGHT)
+            if (direction == Direction.right)
             {
                 x = x + offset;
             }
-            else if (direction == Direction.LEFT)
+            else if (direction == Direction.left)
             {
                 x = x - offset;
             }
-            else if (direction == Direction.UP)
+            else if (direction == Direction.up)
             {
                 y = y - offset;
             }
-            else if (direction == Direction.DOWN)
+            else if (direction == Direction.down)
             {
                 y = y + offset;
             }
         }
-        public bool IsHit(Point p)
+
+        public bool IsHit(Point point)
         {
-            return p.x == this.x && p.y == this.y;
+            return point.x == this.x && point.y == this.y;
         }
 
         public void Draw()
@@ -63,11 +62,14 @@ namespace snake
         public void Clear()
         {
             sym = ' ';
-            Draw(); 
+            Draw();
         }
+
         public override string ToString()
         {
-            return x + "," + y + "," + sym;
+            return x + ", " + y + ", " + sym;
         }
+
     }
 }
+
