401fececd0c41498b7a5f105acd9e206a2a2e352
aleksandrshevchenko4 <62281789+aleksandrshevchenko4@users.noreply.github.com>
2020-06-04 22:29:16 +0300

    Update Sound.cs

--- snake/Sound.cs
+++ snake/Sound.cs
@@ -1,123 +1,47 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-using System.IO;
-using System.Threading;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
 
 namespace Snake
 {
-    class GameManager
+    class Sound
     {
-        int width;
-        int height;
-        string new_game = "Начать игру";
-        string option = "Настройки";
-        string customisation = "Кастомизация";
-        string exit = "Выход";
-        static int shelf = 1;
-        public string option_chosen;
-        string name;
-        public GameManager(int _width, int _height)
+        public System.Windows.Media.MediaPlayer player = new System.Windows.Media.MediaPlayer();
+        public void Play(string link)
         {
-            width = _width;
-            height = _height;
+            System.Windows.Media.MediaPlayer sound = new System.Windows.Media.MediaPlayer();
+            sound.Open(new System.Uri(link));
+            sound.Volume = 100;
+            sound.Play();
         }
-        public void Menu()
+        public void PlayMusic(string link)
         {
-            Console.Clear();
-            Console.ForegroundColor = ConsoleColor.White;
-            MenuControl("");
+            player.Open(new System.Uri(link));
+            player.Volume = 100;
+            player.Play();
         }
-        public void MenuControl(string key)
+        public void Stop()
         {
-            Console.SetCursorPosition(32, 18);
-            Console.WriteLine(new_game);
-            Console.SetCursorPosition(32, 20);
-            Console.WriteLine(option);
-            Console.SetCursorPosition(32, 22);
-            Console.WriteLine(customisation);
-            Console.SetCursorPosition(32, 24);
-            Console.WriteLine(exit);
-            Console.BackgroundColor = ConsoleColor.Blue;
-
-            if (key == "up")
-            {
-                if (shelf != 1)
-                {
-                    shelf--;
-                }
-            } else if (key == "down")
-            {
-                if (shelf != 5)
-                {
-                    shelf++;
-                }
-            }
-
-            switch (shelf)
-            {
-                case 1:
-                    Console.SetCursorPosition(32, 18);
-                    Console.WriteLine(new_game);
-                    option_chosen = "game";
-                    break;
-                case 2:
-                    Console.SetCursorPosition(32, 20);
-                    Console.WriteLine(option);
-                    option_chosen = "options";
-                    break;
-                case 3:
-                    Console.SetCursorPosition(32, 22);
-                    Console.WriteLine(customisation);
-                    option_chosen = "custom";
-                    break;
-                case 4:
-                    Console.SetCursorPosition(32, 24);
-                    Console.WriteLine(exit);
-                    option_chosen = "exit";
-                    break;
-            }
-
-            Console.BackgroundColor = ConsoleColor.Black;
+            player.Stop();
+        }
+        public void Pause()
+        {
+            player.Pause();
         }
 
-        public void Name_asker()
+        public void Unpause()
         {
-            Console.Clear();
-
-            Console.WriteLine("Напишите ваше имя: ");
-
-            try
-            {
-                name = Console.ReadLine();
-
-                Console.WriteLine("Вы ввели: " + name);
-
-                Console.ReadLine();
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Ошибка! Введите правильно!");
-            }
+            player.Play();
         }
 
-        public void Name_writter(int score)
+        public void SetVolume(int vol)
         {
-            Console.Clear();
-            StreamWriter to_file = new StreamWriter("Результаты.txt", true);
-            to_file.WriteLine(name + " - " + score);
-            to_file.Close();
-        }
-
-        public void GameOver()
-        {
-            Sound sound = new Sound();
-            sound.Play(@"C:\Users\Yarik\source\repos\Snake-master\Snake_Game\lose.wav");
-            Console.Clear();
-            Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.SetCursorPosition(38, 16);
-            Console.WriteLine("Игра окончена");
-            Thread.Sleep(1000);
+            Pause();
+            player.Volume = vol;
+            Unpause();
         }
     }
 }
